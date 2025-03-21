522f973859d4dc0c30f0f1927a86560067f5caaf
miranda-beatriz <miranda.beatrizcosta@gmail.com>
2025-03-20 21:24:42 -0300

    creating project

---
+++ controllers/books.js
@@ -0,0 +1,86 @@
+const mongodb = require('../data/database');
+const { ObjectId } = require('mongodb');
+
+const getAll = async (req, res) => {
+      //#swagger.tags=['books']
+  const result = await mongodb.gettDatabase().db().collection('books').find();
+  result.toArray().then((books) => {
+    res.setHeader('content-Type', 'application/json');
+    res.status(200).json(books);
+  });
+};
+
+const getSingle = async (req, res) => {
+      //#swagger.tags=['books']
+  const bookId = new ObjectId(req.params.id);
+  const result = await mongodb.gettDatabase().db().collection('books').find({ _id: bookIdId });
+  result.toArray().then((books) => {
+    res.setHeader('content-Type', 'application/json');
+    res.status(200).json(books[0]);
+  });
+};
+
+const createBook = async (req, res) => {
+      //#swagger.tags=['books']
+    const book = {
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      publication_year: req.body.publication_year,
+      publisher: req.body.publisher,
+      isbn: req.body.isbn,
+      pages: req.body.pages,
+      summary:req.body.summary
+    };
+    const response = await mongodb.gettDatabase().db().collection('books').insertOne(book);
+    if(response.acknowledged){
+      res.status(204).send();
+    } else{
+      res.status(500).json(response.error || 'Some error ocurred while updating the book information');
+    }
+
+};
+
+const updateBook = async (req, res) => {
+      //#swagger.tags=['books']
+  const contactId = new ObjectId(req.params.id);
+  const book = {
+    title: req.body.title,
+    author: req.body.author,
+    genre: req.body.genre,
+    publication_year: req.body.publication_year,
+    publisher: req.body.publisher,
+    isbn: req.body.isbn,
+    pages: req.body.pages,
+    summary:req.body.summary
+  };
+  const response = await mongodb.gettDatabase().db().collection('books').replaceOne({_id:bookId},book);
+  if(response.modifiedCount > 0){
+    res.status(204).send();
+  } else{
+    res.status(500).json(response.error || 'Some error ocurred while updating the book information');
+  }
+
+};
+
+const deleteBook = async (req, res) => {
+      //#swagger.tags=['books']
+    const bookId = new ObjectId(req.params.id);
+    const response = await mongodb.gettDatabase().db().collection('books').deleteOne({_id:bookId});
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else{
+      res.status(500).json(response.error || 'Some error ocurred while deleting the book information.');
+    }
+    
+};
+
+
+module.exports = {
+  getAll,
+  getSingle,
+  createBook,
+  updateBook,
+  deleteBook
+};
+
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+router.use(require('./swagger'));
+
+router.get('/', (req,res) => {
+    //#swagger.tags=['books]
+    res.send('Hello World');
+});
+
+router.use('/books', require('./books'));
+
+module.exports = router;
